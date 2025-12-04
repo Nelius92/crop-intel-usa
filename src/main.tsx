a5c5b0ad1c606d13ad0c90f2568b21576819dd42
Cornelius <cornelius@Corneliuss-MacBook-Pro-2.local>
2025-12-04 01:56:30 -0600

    Fix: Move console.log out of JSX v1.0.2

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,10 @@ import App from './App.tsx'
 import './index.css'
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+console.log("Corn Intel Build: v1.0.2 - Fixes Applied");
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        {console.log("Corn Intel Build: v1.0.1 - Fixes Applied")}
         <App />
     </React.StrictMode>,
 )
